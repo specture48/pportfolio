b6ea7d1efdef7aac67099f3a6a3bb11b99d48de2
daniel99k <dk99@divcodes.com>
2024-11-30 19:49:09 +0300

    fix build issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
 import Bio from "../components/Bio.tsx";
 import Projects from "../components/Projects.tsx";
-import Skills from "../components/Skills.tsx";
 import Footer from "../components/Footer.tsx";
 import Educations from "../components/Educations.tsx";
 import WorkExperiences from "../components/WorkExperiences.tsx";
