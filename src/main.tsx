c170a413a9832e8786b6434d2163227df45b39f3
daniel99k <dk99@divcodes.com>
2024-12-04 22:08:15 +0300

    ..

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import {RouterProvider} from "react-router-dom";
 import './index.css'
 import {routers} from "./routes.tsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <RouterProvider router={routers}/>
 );
